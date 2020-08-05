1c3d1e235a37681d5164ba52bcd40255464f7a3f
sontus-tripura <sontustrp.com@gmail.com>
2020-08-05 15:26:24 +0600

    postgresql added

--- hasansite/settings.py
+++ hasansite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'hasansite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SONTUS',
+        'USER':  'postgres',
+        'PASSWORD': 'programmer2020',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
